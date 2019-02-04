e1d7461413a11f008b9c0b67a590f2d3b56927d1
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-04 17:32:39 +0200

    here it is wrong button click

--- spec/test_scenario/alibaba.rb
+++ spec/test_scenario/alibaba.rb
@@ -9,7 +9,7 @@ feature 'TrueAutomation.IO capybara example' do
     
     find(ta(   'p:alibaba:home:searchButton')).click
     sleep 7
-    find(ta(   'p:alibaba:home:results:likeBtn')).click
+    find(ta(   'p:alibaba:home:results:likeBtn')).click  // wrong button click
     
     sleep 2
   end
