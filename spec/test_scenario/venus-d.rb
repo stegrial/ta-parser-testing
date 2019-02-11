9a02919cdff3176b368c2c5210b0f27a592fdea2
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-11 19:19:52 +0200

    added for TA-688

--- spec/test_scenario/venus-d.rb
+++ spec/test_scenario/venus-d.rb
@@ -14,18 +14,44 @@ feature 'TrueAutomation.IO capybara example' do
 
     visit 'https://venus-preprod.moovweb.net/'
 
-    find(:xpath, ta("profileBtn")).click
-    find(:xpath, ta("emailFld")).set('qateam@moovweb.com')
-    find(:xpath, ta("passwordFld")).set('1234567s')
-    find(:xpath, ta("signIn")).click
+    #find(:xpath, ta("profileBtn")).click
+    find(:xpath, ('/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]')).click
+
+    #find(:xpath, ta("emailFld")).set('qateam@moovweb.com')
+    #find(:xpath, ta("emailFld", "//input[@placeholder='Email']")).set('qateam@moovweb.com')
+    find(:xpath, ("//input[@placeholder='Email']")).set('qateam@moovweb.com')
+
+    #find(:xpath, ta("passwordFld")).set('1234567s')
+    #find(:xpath, ta("passwordFld","//input[@id='ctl00_Body1_loginpassword']")).set('1234567s')
+    find(:xpath, ("//input[@id='ctl00_Body1_loginpassword']")).set('1234567s')
+
+    # find(:xpath, ta("signIn")).click
+    #find(:xpath, ta("signIn","//a[@id='ctl00_Body1_loginbutton']")).click
+    find(:xpath, ("//a[@id='ctl00_Body1_loginbutton']")).click
+
+    #find(:xpath, ta("profileBtn")).click
+    find(:xpath, ("/html[1]/body[1]/form[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]")).click
+
+
+    #find(:xpath, ta("account_info")).click
+    #find(:xpath, ta("account_info", "//div[6]//div[2]//ul[1]//li[1]")).click
+    find(:xpath, ("//div[6]//div[2]//ul[1]//li[1]")).click
+
+    #find(:xpath, ta("profileBtn")).click
+    find(:xpath, ("/html[1]/body[1]/form[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]")).click
+
+
+    #find(:xpath, ta("wish_list", "//div[6]//div[2]//ul[1]//li[4]")).click
+    find(:xpath, ("//div[6]//div[2]//ul[1]//li[4]")).click
+    sleep 5
+
+    # a = find(:xpath, ("(//button[@aria-label='Account'])[1]"))
+    # puts a.path.downcase
+
     find(:xpath, ta("profileBtn")).click
 
-    find(:xpath, ta("account_info")).click
-    find(:xpath, ta("profileBtn")).click
-    find(:xpath, ta("wish_list")).click
-    sleep 5
-    find(:xpath, ta("profileBtn")).click
-    find(:xpath, ta("account_info")).click
+    # #find(:xpath, ta("account_info","//div[6]//div[2]//ul[1]//li[1]")).click
+    # find(:xpath, ("//div[6]//div[2]//ul[1]//li[1]")).click
     sleep 5
   end
 end
