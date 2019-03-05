4299f187b511a2a27e5e1547df8f3ce5564b73e2
Grigoriy Stetsenko <stetsenko@softesis.com>
2019-03-05 15:28:52 +0200

    adding test: Venus - Clicking by the Account menu items

--- help-tools/address_converter.rb
+++ help-tools/address_converter.rb
@@ -1,11 +1,12 @@
-address = "0.1.4.0.2.0.0.0.0.0.10.0"
+address = "/*[1]/*[2]/*[40]/*[2]/*[1]/*[3]"
 
-  array = address.split(".")
-  for i in array
-  print "/*[#{i.to_i + 1}]"
-  end
-
-  # array = address.split("/*[")
+  # array = address.split(".")
   # for i in array
   # print "/*[#{i.to_i + 1}]"
   # end
+
+  array = address.split("/*[")
+  for i in array
+    next if i == array[0]
+    print "#{(i[0...-1]).to_i - 1}."
+  end
